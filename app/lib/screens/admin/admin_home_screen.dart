e266c3255568000847f36217b007369c0a3755ad
Rao Shab <raoshab@192.168.0.190>
2026-07-05 14:20:45 +0530

    fix(flutter): Import LoggerService in AdminHomeScreen

--- app/lib/screens/admin/admin_home_screen.dart
+++ app/lib/screens/admin/admin_home_screen.dart
@@ -13,6 +13,8 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../l10n/locale_keys.g.dart';
 import 'package:kaamkaaz/widgets/voice_text_field.dart';
+import '../../services/logger_service.dart';
+
 class AdminHomeScreen extends ConsumerStatefulWidget {
   const AdminHomeScreen({super.key});
 
